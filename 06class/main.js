1907e82aa682d2fc99e4b328fd584c0358739626
vikas <v8kgsm@gmail.com>
2021-12-13 10:34:49 +0530

    class

---
+++ 06class/main.js
@@ -0,0 +1,11 @@
+var point = /** @class */ (function () {
+    function point() {
+    }
+    point.prototype.draw = function () {
+        // code...
+    };
+    point.prototype.calculateDistance = function (anotherPoint) {
+        // code...
+    };
+    return point;
+}());
---
+++ 06class/main.ts
@@ -0,0 +1,10 @@
+class point{
+    x: number;
+    y: number;
+    draw() {
+        // code...
+     }
+    calculateDistance(anotherPoint: Point) {
+        // code...
+    }
+}
