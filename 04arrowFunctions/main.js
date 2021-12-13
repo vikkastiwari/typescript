2ee848d8913f40d855299cab5898052fdd351e70
vikas <v8kgsm@gmail.com>
2021-12-13 10:00:07 +0530

    arrow functions

---
+++ 04arrowFunctions/main.js
@@ -0,0 +1,7 @@
+var res = function (message) {
+    console.log(message);
+};
+// arrow function
+var res1 = function (message) {
+    console.log(message);
+};
---
+++ 04arrowFunctions/main.ts
@@ -0,0 +1,8 @@
+let res = function ( message ) {
+    console.log(message);
+}
+
+// arrow function
+let res1 = ( message ) => {
+    console.log(message);
+}
