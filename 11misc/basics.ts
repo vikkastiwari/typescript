9d3417593335ac0f1d2010403df243c449fca629
vikas <v8kgsm@gmail.com>
2022-01-26 09:38:18 +0530

    string,number,boolean

---
+++ 11misc/basics.ts
@@ -0,0 +1,11 @@
+function add(num1:number,num2:number) {
+    return num1 + num2;
+}
+
+// type assignment
+let number1: number;
+number1 = 4;
+
+// when you hover ts infere's the type of the variable
+const number2 = 4;
+let number3 = '4';
--- 11misc/misc.ts
+++ 11misc/misc.ts
@@ -1,7 +1,7 @@
 const button = document.querySelector( "button" );
 const input1 = document.getElementById( "num1" )! as HTMLInputElement;
 const input2 = document.getElementById( "num2" )! as HTMLInputElement;
-// "!" we write it when we are assure that the id's exists in html page
+// ! we write it when we are assure that the id's exists in html page
 // by writing HTMLInputElement we are typecasting it
 function add( num1:number, num2:number ) {
     return num1 + num2;
