509649d9fca0ab956947c8b56486be06a550b478
vikas <v8kgsm@gmail.com>
2022-01-26 13:34:33 +0530

    unknown & never types

---
+++ 11misc/function-types.ts
@@ -0,0 +1,40 @@
+// function return types
+
+// specified type
+function add(val:number): number | string{
+    return val;
+}
+
+// undefined type
+function sub(): undefined{
+    return;
+}
+
+// void type
+function print(): void{
+    console.log("void type");
+}
+
+// function types
+
+function combo( num1: number ) { console.log( num1 ); return 0; }
+
+let callFunction:Function; // defining function type
+callFunction = combo;
+
+// other of defining return type using callback
+// it takes a number and returns a number
+let callFunction2: (a:number) => number;
+callFunction2 = combo;
+
+
+// CALLBACKS & FUNCTION TYPES
+function addAndHandle( n1: number, n2: number, cb: ( num: number ) => void ) {
+    const result = n1 + n2;
+    cb( result );
+}
+
+addAndHandle( 1, 2, ( result ) => {
+    console.log(result);
+    
+})
