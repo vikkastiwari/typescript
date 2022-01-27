f0afbc1c6beae6ab857bbdb62821420bcd2d55e8
Vikas Tiwari <51874681+vikkastiwari@users.noreply.github.com>
2022-01-27 10:13:37 +0530

    class

--- 06class/main.js
+++ 06class/main.js
@@ -1,14 +1,13 @@
 var Point = /** @class */ (function () {
-    function Point() {
-    }
-    Point.prototype.draw = function () {
-        console.log('X: ' + this.x + ', Y: ' + this.y);
-    };
-    Point.prototype.calculateDistance = function (anotherPoint) {
-        // code...
-    };
-    return Point;
-}());
+  function Point() {}
+  Point.prototype.draw = function () {
+    console.log("X: " + this.x + ", Y: " + this.y);
+  };
+  Point.prototype.calculateDistance = function (anotherPoint) {
+    // code...
+  };
+  return Point;
+})();
 var pointObject = new Point();
 pointObject.x = 1;
 pointObject.y = 2;
