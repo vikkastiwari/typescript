94ce5cb2214e73e83aaa62a916de21a3feb624d2
vikas <v8kgsm@gmail.com>
2022-01-29 10:28:41 +0530

    type guards

--- 12advancedTypes/src/type-guards.ts
+++ 12advancedTypes/src/type-guards.ts
@@ -5,3 +5,63 @@ function add( a: Combinable, b: Combinable ) {
     }
     return a + b;
 }
+
+// custom types
+type Emp = {
+    name: string;
+    startDate: string;
+}
+type Admin = {
+    name: string;
+    privilege: string;
+}
+
+// setting guard 
+function print( user: Emp | Admin ) {
+    console.log(user.name);
+    
+    if ( "startDate" in user )
+        console.log( "Start Date: " + user.startDate);
+
+    if ( "privilege" in user )
+        console.log("Privileges:" + user.privilege);
+}
+
+const user: Admin = {
+    name: "Vikas",
+    privilege:"Host"
+}
+
+print( user );
+
+// Type guard on class
+class Car{
+    drive() {
+        console.log("Driving Car");
+    }
+}
+
+class Truck{
+    drive() {
+        console.log("Driving Truck");
+    }
+    cargo(amount:number) {
+        console.log("Loading cargo:"+amount);
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle:Vehicle) {
+    vehicle.drive();
+
+    // type guard on class
+    if ( vehicle instanceof Truck ) {
+        vehicle.cargo( 100 );
+    }
+}
+
+useVehicle(v2);
